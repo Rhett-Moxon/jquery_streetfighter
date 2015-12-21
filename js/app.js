6a264354ef0e33aa9e5a09d05aad9f825c49ac75
Rhett-Moxon <rhett.git@gmail.com>
2015-12-20 21:21:56 -0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
 $(document).ready(function() {
 
-	$("#images").css("background-image", "url(images/still.png)");
+	$("#images")
+		.css("background-image", "url(images/ready.gif)")
+		.css('cursor','pointer')
+		.mousedown(function(){
+			$(this).css("background-image", "url(images/cool.gif)");
+		})
+		.mouseup(function(){
+			$(this).css("background-image", "url(images/ready.gif)");
+		});
 
-	$("#instruct div p").css("color", "green");
+	$(document)
+		.keydown(function(e){
+			if(e.which == 32){
+				playBall();
+				$("#images")
+					.css("background-image", "url(images/throw.png)");
+				$("#ball")
+					.finish()
+					.show()
+					.animate({'left':'835px'},500,function(){
+						$(this).hide();
+						$(this).css('left', '520px')
+					});
+			}
+		})
+		.keyup(function(e){
+			if(e.which == 32){
+				$("#images")
+					.css("background-image", "url(images/ready.gif)");
+				$("#ball")
+					.hide();
+			}
+		});
 });
+
+function playBall() {
+  $('#ball_sound')[0].volume = 0.5;
+  $('#ball_sound')[0].load();
+  $('#ball_sound')[0].play();
+}
