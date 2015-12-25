466cb5545becaf334099c17afd194f39507c0d01
Rhett-Moxon <rhett.git@gmail.com>
2015-12-25 17:12:49 -0500

    put staton changes in merged files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /* "$(function(){});" is shorthand for "$(document).ready(function(){});". This is known a "document ready event". The purpose of the document ready event is to prevent any jQuery code from running before the document is finished loading (is ready). */
 
@@ -104,47 +103,3 @@ $(function() {
 
   }
 });
-=======
-$(document).ready(function() {
-
-	$("#images")
-		.css("background-image", "url(images/ready.gif)")
-		.css('cursor','pointer')
-		.mousedown(function(){
-			$(this).css("background-image", "url(images/cool.gif)");
-		})
-		.mouseup(function(){
-			$(this).css("background-image", "url(images/ready.gif)");
-		});
-
-	$(document)
-		.keydown(function(e){
-			if(e.which == 32){
-				playBall();
-				$("#images")
-					.css("background-image", "url(images/throw.png)");
-				$("#ball")
-					.finish()
-					.show()
-					.animate({'left':'835px'},500,function(){
-						$(this).hide();
-						$(this).css('left', '520px')
-					});
-			}
-		})
-		.keyup(function(e){
-			if(e.which == 32){
-				$("#images")
-					.css("background-image", "url(images/ready.gif)");
-				$("#ball")
-					.hide();
-			}
-		});
-});
-
-function playBall() {
-  $('#ball_sound')[0].volume = 0.5;
-  $('#ball_sound')[0].load();
-  $('#ball_sound')[0].play();
-}
->>>>>>> 6a264354ef0e33aa9e5a09d05aad9f825c49ac75
